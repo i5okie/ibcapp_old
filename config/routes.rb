be37ab23ff9990b5492727c30b28937add4602ed
Ivan Polchenko <ipzent@gmail.com>
2013-09-09 09:34:06 -0700

    fixed routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Ibcapp::Application.routes.draw do
 
   resources :items, only: :index
 
-  root :to => 'home#index', as: nil
+  root :to => 'home#index'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
